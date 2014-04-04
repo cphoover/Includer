b989c631d71b97630d8c3bb66e294c14ed445609
Charlie Hoover <cphoover@gmail.com>
2014-04-04 16:39:49 -0400

    made the second one an actual test

--- tests/TestRunner.js
+++ tests/TestRunner.js
@@ -9,5 +9,33 @@ exec('node ' + __dirname + '/app1.js', function(_err, _stdout, _stderr){
 });
 
 exec('node ' + __dirname + '/app2.js', function(_err, _stdout, _stderr){
-    console.log(_stdout);
+    var testStr = [
+        '<!DOCTYPE html>',
+        '<html lang="en">',
+        '<head>',
+        '    <meta charset="UTF-8">',
+        '    <title>TestFile</title>',
+        '</head>',
+        '<body>',
+        '    <header>',
+        '        <nav>',
+        '            <ul>',
+        '                <li>Link 1</li>',
+        '                <li>Link 2</li>',
+        '                <li>Link 3</li>',
+        '                <li>Link 4</li>',
+        '                <li>Link 5</li>',
+        '                <li>Link 6</li>',
+        '                <li>Link 7</li>',
+        '                <li>Link 8</li>',
+        '                <li>Link 9</li>',
+        '            </ul>',
+        '        </nav>',
+        '    </header>',
+        '</body>',
+        '</html>'
+    ].join("\n");
+
+    console.log("testing:\n[" + testStr + "]\n===\n[" + _stdout + "]");
+    assert(_stdout === testStr, "should be equal");
 });
