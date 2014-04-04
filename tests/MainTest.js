1516a6028412373abf0419a5c27f2be0486ab9ec
Charlie Hoover <cphoover@gmail.com>
2014-04-04 09:41:18 -0400

    adding more visual output to test

--- tests/MainTest.js
+++ tests/MainTest.js
@@ -3,6 +3,7 @@ var assert = require('assert');
 
 exec('node ' + __dirname + '/app1.js', function(_err, _stdout, _stderr){
     var testStr = ["1","2","3","4","MIDDLE","4","3","2","1","DONE"].join("\n");
+    console.log("testing:\n[" + testStr + "]\n===\n[" + _stdout + "]");
     assert(_stdout === testStr);
 });
 
